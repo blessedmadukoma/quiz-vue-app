1212d1a4de11416c24e6bb7c14a704e0cf953d0e
blessedmadukoma <blessedmadukoma@gmail.com>
2023-08-16 20:28:51 +0100

    added question status, computed property for changing question status

--- src/components/QuizHeader.vue
+++ src/components/QuizHeader.vue
@@ -1,7 +1,14 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const { questionStatus } = defineProps(['questionStatus'])
+
+</script>
+
 <template>
  <div>
   <header>
-   <h4>Question 1/3</h4>
+   <h4>Question {{ questionStatus }}</h4>
    <div class="bar">
     <div class="completion"></div>
    </div>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue"
 import quizes from "../data/quizes.json";
@@ -14,15 +14,28 @@ const quiz = quizes.find(q => q.id === quizId)
 
 const currentQuestionIndex = ref(0);
 
+// One way -> too many lines of code
+// // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+// // watch and change the question status once the next button is clicked i.e. the current question index value changes
+// watch(() => currentQuestionIndex.value, () => {
+// 	questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+// })
+
+// another way -> shorter line of code
+// computed -> compute a value based off on another value i.e. compute questionStatus based off the currentQuestionIndex.value
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
 </script>
 
 <template>
 	<div>
-		<QuizHeader />
+		<QuizHeader :questionStatus="questionStatus" />
 
 		<div>
 			<Question :question="quiz.questions[currentQuestionIndex]" />
 		</div>
+
+		<button @click="currentQuestionIndex++">Next Question</button>
 	</div>
 </template>
 
